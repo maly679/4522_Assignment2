915103e14b15de444fa5bffa1854657a473e0cf6
matt <nptsm@protonmail.com>
2022-03-23 23:06:32 -0600

    Post comment to default ID. Message is a concat of msgbox, name, and email. Need to add funcitonality for comment on coin (incorporate the ID dynamically)

--- postComment.js
+++ postComment.js
@@ -1,11 +1,14 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';  // uncomment for local node.js test
 
 const url = 'https://sqldatacoins.azurewebsites.net/api/coins';
 
-function put_comment(id, message){
+function put_comment(id){
+    var message = document.getElementById("comment-box").value
+    var name = document.getElementById("fullname").value
+    var email = document.getElementById("email").value
     var comment_start = "{\"Comments\": \""
     const put_url = url.concat("/",id)
-    const comment = comment_start.concat(message,"\"}")
+    const comment = comment_start.concat(message," - ",name,": ",email,"\"}")
 
     fetch(put_url, {
         method: 'PUT',
@@ -20,7 +23,7 @@ function put_comment(id, message){
         })
 }
 
-//test
-var id = "6235300c7d843b7d8b633361"
-var msg = "its a test"
-put_comment(id, msg)
+//test values/call
+// var id = "6235300c7d843b7d8b633361"
+// var msg = "node test from cli"
+// put_comment(id, msg)
