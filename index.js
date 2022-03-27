a821e12fc58ef29692ab0d790d6fcfd6d56ef21c
Moahmed Aly <maly679@mtroyal.ca>
2022-03-27 15:56:34 -0600

    Fixing issues with async fetch and adding DELETE for a comment's coind

--- index.js
+++ index.js
@@ -2,71 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const mainCoins = 'https://sqldatacoins.azurewebsites.net/api/HttpTrigger3?code=Rwc2/lGDvUiJ9DnDqLXYLLGcP41IB2eSnFMA8SGIyXLhFvjon00elw==';
 const CryptoCoins = 'https://sqldatacoins.azurewebsites.net/api/CryptoInfo';
-const CoinPut = 'https://sqldatacoins.azurewebsites.net/api/coins/id ';
+const CoinPut = 'https://sqldatacoins.azurewebsites.net/api/coins/id';
 let glist;
-
-Promise.all([
-    fetch(mainCoins),
-    fetch(CryptoCoins),
-    fetch(CoinPut)
-  ]).then(allResponses => {
-    const response1 = allResponses[0]
-    const response2 = allResponses[1]
-    const response3 = allResponses[2]
-  
-    response1.json().then(data => {
-        console.log("yes");
-        glist = [...data];
-
-    });
-
-
-        response2.json().then(data => {
-
-    
-              console.log(data);
-                   for (let d of data) {
-                       for (let g of glist) {
-                   if (d.Symbol == g.CoinSymbol) {
-                       g.Price = d.Marketcap;
-                       g.Date = d.Date.split("23:59")[0];
-                       
-                   }
-                }
-              
-            }
-      
-         
-        });
-        response3.json().then(data => {
-
-            for (let d of data) {
-                for (let g of glist) {
-                    console.log(d.Symbol);
-            if (d.Symbol == g.CoinSymbol) {
-                g.Comments = d.selectedComment;
-                g.ID = d._id;
-                console.log(g.ID);
-            }
-         }
-       
-        
-     }
-     addCoinList(glist);
-     console.log(glist);
-
-        });
-
-      });
-    
-
 let selectedCoinValue;
 let selectedComment;
 
 
-function addCoinList(data) {
-
-    console.log(data);
+ function addCoinList(data) {
 
     document.querySelector(".coin").innerHTML = "";
     let ulStuff = document.getElementsByClassName(".coin");
@@ -74,7 +16,6 @@ function addCoinList(data) {
         let optionT = document.createElement("li");
         optionT.textContent = d.CoinName;
         document.querySelector(".coin").appendChild(optionT);
-
 
     }
 
@@ -112,12 +53,17 @@ function addCoinList(data) {
    
     for (let d of data) {
         let optionT = document.createElement("li");
+        optionT.classList.add("deleteOption");
+        
+        let button = document.createElement("button");
         if ( d.Comments)  {
-        optionT.textContent = d.Comments;
+            optionT.innerHTML = d.Comments + ' <button class = "deleteButton"><img class = "exitButton" src="./exitButton.png" /></button> </li>';
+            optionT.setAttribute("id", d.ID);
         } else {
             optionT.innerHTML = "</br>";
         }
         document.querySelector(".comments").appendChild(optionT);
+        
 
     }
 
@@ -132,10 +78,97 @@ function addCoinList(data) {
     }
 
 }
+ async function renderMainCoins() {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+       fetch(mainCoins).then(response => response.json()).then(data => {
+        console.log(data);
+        glist = [...data];
+       });
 
+       resolve();
+    }, 500)
+ });
+}
+
+ 
+ async function renderCryptoCoins() {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+    fetch(CryptoCoins).then(response => response.json()).then(data => {
+
+    
+        console.log(data);
+             for (let d of data) {
+                 for (let g of glist) {
+             if (d.Symbol == g.CoinSymbol) {
+                 g.Price = d.Marketcap;
+                 g.Date = d.Date.split("23:59")[0];
+                 
+             }
+          }
+        }
+
+      });
+      resolve();
+    }, 500)
+});
+}
+
+
+async function renderCoinPut() {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+    fetch(CoinPut).then(response => response.json()).then(data => {
+
+            for (let d of data) {
+
+                for (let g of glist) {
+                    console.log(d.Symbol);
+            if (d.Symbol == g.CoinSymbol) {
+                g.Comments = d.selectedComment;
+                g.ID = d._id;
+                console.log(g.ID);
+            }
+         }
+       
+        
+     }
+     addCoinList(glist);
+
+    });
+    resolve();
+  }, 500)
+});  
+ 
+}
+
+
+
+
+async function inParallel() {
+    const promise1 = renderMainCoins();
+    const promise2 = renderCryptoCoins();
+    const promise3 = renderCoinPut();
+    const first = await promise1;
+    const second = await promise2;
+    const third = await promise3;
+
+    // const addCoin = await addCoinList(glist);
+ 
+
+ }
+
+// renderMainCoins();
+// renderCryptoCoins();
+//  renderCoinPut();
+inParallel();
+ console.log(glist);
+ 
 
 document.querySelector("#selectCoin").addEventListener("change", function(event) {
-
+    // console.log(document.querySelector("#selectCoin"));
+    // var selectedOption = document.forms[0].selectCoin;
 
     selectedCoinValue = event.target.value;
     console.log(selectedCoinValue);
@@ -150,7 +183,34 @@ document.querySelector("#comment-box").addEventListener("change", function (even
 
 
 });
-let url = `https://sqldatacoins.azurewebsites.net/api/coins/${selectedCoinValue}`;
+
+
+
+document.querySelector(".comments").addEventListener('click',  function(e) {
+
+    e.preventDefault();
+
+        if(e.target.className == 'exitButton') {
+    
+    let IDofDeletedCoin = e.target.parentElement.parentElement.id;
+    console.log(IDofDeletedCoin);
+
+        fetch(`https://sqldatacoins.azurewebsites.net/api/coins/${IDofDeletedCoin}`, {
+  method: 'DELETE',
+})
+.then(res => res.text()) // or res.json()
+.then(res => console.log(res))
+
+        }
+    });
+        
+
+
+
+
+
+
+
 document.querySelector(".submitCoin").addEventListener("click",  function(e) {
 
     e.preventDefault();
